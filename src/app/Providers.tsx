aefc00e44762fa6e664e37f653e3932bcee5a99f
kang <kangju2000@naver.com>
2023-09-24 20:08:06 +0900

    :wrench: chakra-ui/next-js 라이브러리 추가

--- src/app/Providers.tsx
+++ src/app/Providers.tsx
@@ -1,10 +1,15 @@
 'use client'
+import { CacheProvider } from '@chakra-ui/next-js'
 import { ChakraProvider } from '@chakra-ui/react'
 
 import type { PropsWithChildren } from 'react'
 
 const Providers = ({ children }: PropsWithChildren) => {
-  return <ChakraProvider>{children}</ChakraProvider>
+  return (
+    <CacheProvider>
+      <ChakraProvider>{children}</ChakraProvider>
+    </CacheProvider>
+  )
 }
 
 export default Providers
