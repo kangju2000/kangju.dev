4b0ed1caab38a552987400d696a0ea92582ab68e
kang <kangju2000@naver.com>
2023-09-25 20:00:28 +0900

    :sparkles: Youtube 컴포넌트 구현

---
+++ src/components/Youtube/Youtube.tsx
@@ -0,0 +1,27 @@
+import { chakra } from '@chakra-ui/react'
+
+interface YoutubeProps {
+  id: string
+}
+
+const Youtube = ({ id }: YoutubeProps) => {
+  return (
+    <figure>
+      <chakra.iframe
+        title="YouTube"
+        src={`https://www.youtube.com/embed/${id}`}
+        w="100%"
+        maxH="400px"
+        aspectRatio="16/9"
+        margin="16px auto"
+        display="flex"
+        justifyContent="center"
+        alignItems="center"
+        borderRadius="8px"
+        border="2px solid var(--chakra-colors-gray-600)"
+      ></chakra.iframe>
+    </figure>
+  )
+}
+
+export default Youtube
