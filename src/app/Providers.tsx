36a8265c2dace839334af797e298cf70d403b799
kang <kangju2000@naver.com>
2023-09-27 02:56:39 +0900

    :heavy_plus_sign: chakra-ui-prose 라이브러리 추가

--- src/app/Providers.tsx
+++ src/app/Providers.tsx
@@ -1,6 +1,7 @@
 'use client'
 import { CacheProvider } from '@chakra-ui/next-js'
 import { ChakraProvider, extendTheme } from '@chakra-ui/react'
+import { withProse } from '@nikolovlazar/chakra-ui-prose'
 import { Noto_Sans_KR } from 'next/font/google'
 
 import type { PropsWithChildren } from 'react'
@@ -11,12 +12,30 @@ const notoSansKr = Noto_Sans_KR({
   subsets: ['latin'],
 })
 
-const theme = extendTheme({
-  fonts: {
-    heading: notoSansKr.style.fontFamily,
-    body: notoSansKr.style.fontFamily,
+const theme = extendTheme(
+  {
+    fonts: {
+      heading: notoSansKr.style.fontFamily,
+      body: notoSansKr.style.fontFamily,
+    },
   },
-})
+  withProse({
+    baseStyle: {
+      h2: {
+        fontSize: '3xl',
+      },
+      h3: {
+        fontSize: '2xl',
+      },
+      h4: {
+        fontSize: 'xl',
+      },
+      li: {
+        listStyleType: 'disc',
+      },
+    },
+  })
+)
 
 const Providers = ({ children }: PropsWithChildren) => {
   return (
--- src/layouts/MDXLayout.tsx
+++ src/layouts/MDXLayout.tsx
@@ -1,21 +1,15 @@
 'use client'
-import { Box, Divider, Heading, Text } from '@chakra-ui/react'
-import { DocumentTypes } from 'contentlayer/generated'
+
+import { Heading } from '@chakra-ui/react'
+import { Prose } from '@nikolovlazar/chakra-ui-prose'
 import { MDXComponents } from 'mdx/types'
 import { useMDXComponent } from 'next-contentlayer/hooks'
 
 import Youtube from '@/components/Youtube/Youtube'
 
+import type { DocumentTypes } from '.contentlayer/generated/types'
+
 const components: MDXComponents = {
-  h1: (props) => <Text as="h1" fontSize="5xl" fontWeight="bold" mt="32px" mb="24px" {...props} />,
-  h2: (props) => <Text as="h2" fontSize="3xl" fontWeight="bold" mt="24px" mb="18px" {...props} />,
-  h3: (props) => <Text as="h3" fontSize="2xl" fontWeight="bold" mt="18px" mb="16px" {...props} />,
-  h4: (props) => <Text as="h4" fontSize="xl" fontWeight="bold" mt="16px" mb="14px" {...props} />,
-  h5: (props) => <Text as="h5" fontSize="lg" fontWeight="bold" {...props} />,
-  h6: (props) => <Text as="h6" fontSize="md" fontWeight="bold" {...props} />,
-  p: (props) => <Text as="p" fontSize="md" {...props} />,
-  hr: (props) => <Divider my={4} {...props} />,
-  br: ({ ...props }) => <Box as="br" h="24px" {...props} />,
   Youtube,
 }
 
@@ -31,7 +25,9 @@ const MDXLayout = ({ content }: MDXLayoutProps) => {
       <Heading as="h1" fontSize="5xl" fontWeight="bold" mb="24px">
         {content.title}
       </Heading>
-      <MDXComponent components={components} />
+      <Prose>
+        <MDXComponent components={components} />
+      </Prose>
     </div>
   )
 }
