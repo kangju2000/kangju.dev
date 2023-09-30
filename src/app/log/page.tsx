43959370f7376ab3f6c89c0ce9dea53e86961707
kang <kangju2000@naver.com>
2023-10-01 04:50:16 +0900

    :sparkles: 로그 페이지에 캘린더 적용

--- src/app/log/page.tsx
+++ src/app/log/page.tsx
@@ -1,48 +1,20 @@
 import { Center, Heading, Spacer, Stack } from '@chakra-ui/react'
-import fs from 'fs'
-import Link from 'next/link'
 
-import List from '@/components/List/List'
+import Calendar from '@/components/Calendar/Calendar'
+import ChakraMotion from '@/components/ChakraMotion/ChakraMotion'
 
 export default function LogPage() {
-  const monthList = fs.readdirSync('contents/log')
-
   return (
-    <Stack h="100%">
-      <Center>
-        <Heading as="h2" fontSize="3xl">
-          개발 로그
-        </Heading>
-      </Center>
-      <Spacer />
-      <List
-        direction="row"
-        gap="12px"
-        justifyContent="space-between"
-        overflow="hidden"
-        overflowX="scroll"
-        items={monthList}
-        renderItem={(month) => (
-          <Link href={`/log/${month}`}>
-            <Center
-              as="li"
-              w="100px"
-              h="120px"
-              borderRadius="16px"
-              flexShrink="0"
-              cursor="pointer"
-              _dark={{
-                bg: 'gray.600',
-              }}
-              _light={{
-                bg: 'gray.200',
-              }}
-            >
-              {month}
-            </Center>
-          </Link>
-        )}
-      />
-    </Stack>
+    <ChakraMotion initial={{ opacity: 0 }} animate={{ opacity: 1 }} exit={{ opacity: 0 }}>
+      <Stack h="100%">
+        <Center>
+          <Heading as="h2" fontSize="3xl">
+            개발 로그
+          </Heading>
+        </Center>
+        <Spacer />
+        <Calendar />
+      </Stack>
+    </ChakraMotion>
   )
 }
