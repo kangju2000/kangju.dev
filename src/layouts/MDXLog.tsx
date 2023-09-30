5f8a6648243211824e0bc47c02e32fc611d283fe
kang <kangju2000@naver.com>
2023-10-01 04:53:21 +0900

    :lipstick: MDX layout에 애니메이션 추가

--- src/layouts/MDXLog.tsx
+++ src/layouts/MDXLog.tsx
@@ -2,6 +2,7 @@ import { Center, Heading } from '@chakra-ui/react'
 import { format } from 'date-fns'
 
 import MDXContent from './MDXContent'
+import ChakraMotion from '@/components/ChakraMotion/ChakraMotion'
 
 import type { Log } from 'contentlayer/generated'
 
@@ -12,12 +13,21 @@ interface MDXLogProps {
 const MDXLog = ({ log }: MDXLogProps) => {
   return (
     <div>
-      <Center>
-        <Heading as="h1" fontSize="5xl" fontWeight="bold" mb="24px">
-          {format(new Date(log.date), 'yyyy년 MM월 dd일')}
-        </Heading>
-      </Center>
-      <MDXContent code={log.body.code} />
+      <ChakraMotion initial={{ opacity: 0 }} animate={{ opacity: 1 }} exit={{ opacity: 0 }}>
+        <Center>
+          <Heading as="h1" fontSize="5xl" fontWeight="bold" mb="24px">
+            {format(new Date(log.date), 'yyyy년 MM월 dd일')}
+          </Heading>
+        </Center>
+      </ChakraMotion>
+      <ChakraMotion
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        exit={{ opacity: 0 }}
+        transition={{ delay: 0.2 }}
+      >
+        <MDXContent code={log.body.code} />
+      </ChakraMotion>
     </div>
   )
 }
--- src/layouts/MDXPost.tsx
+++ src/layouts/MDXPost.tsx
@@ -2,6 +2,7 @@ import { Box, Center, Flex, Heading, Stack } from '@chakra-ui/react'
 import Image from 'next/image'
 
 import MDXContent from './MDXContent'
+import ChakraMotion from '@/components/ChakraMotion/ChakraMotion'
 
 import type { Post } from 'contentlayer/generated'
 
@@ -12,47 +13,58 @@ interface MDXPostProps {
 const MDXPost = ({ post }: MDXPostProps) => {
   return (
     <Stack spacing="50px">
-      <Center
-        pos="relative"
-        w="100%"
-        minH="200px"
-        p="24px"
-        flexDirection="column"
-        borderRadius="16px"
-        overflow="hidden"
-      >
-        <Box pos="absolute" top="0" left="0" w="100%" h="100%" bg="blackAlpha.500" zIndex="1" />
-        <Image
-          src={post.thumbnail}
-          alt="thumbnail"
-          style={{
-            objectFit: 'cover',
-          }}
-          fill
-        />
-        <Center flexDirection="column" zIndex="1" h="100%" color="white" textAlign="center">
-          <Heading
-            as="h1"
-            fontSize={{
-              base: '3xl',
-              sm: '5xl',
+      <ChakraMotion initial={{ opacity: 0 }} animate={{ opacity: 1 }} exit={{ opacity: 0 }}>
+        <Center
+          pos="relative"
+          w="100%"
+          minH="200px"
+          p="24px"
+          flexDirection="column"
+          borderRadius="16px"
+          overflow="hidden"
+        >
+          <Box pos="absolute" top="0" left="0" w="100%" h="100%" bg="blackAlpha.500" zIndex="1" />
+          <Image
+            src={post.thumbnail}
+            alt="thumbnail"
+            style={{
+              objectFit: 'cover',
             }}
-            fontWeight="bold"
-            mb={{
-              base: '12px',
-              sm: '18px',
-            }}
-            wordBreak="break-all"
-          >
-            {post.title}
-          </Heading>
-          <Flex>
-            {post.dateFormatted} | {post.readTime.text}
-          </Flex>
+            fill
+          />
+          <Center flexDirection="column" zIndex="1" h="100%" color="white" textAlign="center">
+            <Heading
+              as="h1"
+              fontSize={{
+                base: '3xl',
+                sm: '5xl',
+              }}
+              fontWeight="bold"
+              mb={{
+                base: '12px',
+                sm: '18px',
+              }}
+              wordBreak="break-all"
+            >
+              {post.title}
+            </Heading>
+            <Flex>
+              {post.dateFormatted} | {post.readTime.text}
+            </Flex>
+          </Center>
         </Center>
-      </Center>
+      </ChakraMotion>
 
-      <MDXContent code={post.body.code} />
+      <ChakraMotion
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        exit={{ opacity: 0 }}
+        transition={{
+          delay: 0.4,
+        }}
+      >
+        <MDXContent code={post.body.code} />
+      </ChakraMotion>
     </Stack>
   )
 }
