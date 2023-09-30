723fca449c2c8a0fc116c05ad30d76cea7699a6b
kang <kangju2000@naver.com>
2023-10-01 04:50:46 +0900

    :sparkles: posts 페이지에 페이지 애니메이션 추가

--- src/app/posts/page.tsx
+++ src/app/posts/page.tsx
@@ -2,17 +2,20 @@ import { Stack } from '@chakra-ui/react'
 import { allPosts } from 'contentlayer/generated'
 
 import PostCard from './components/PostCard'
+import ChakraMotion from '@/components/ChakraMotion/ChakraMotion'
 import List from '@/components/List/List'
 
 export default function PostsPage() {
   return (
-    <Stack>
-      <List
-        gap="24px"
-        items={allPosts}
-        renderItem={(post) => <PostCard key={post.slug} post={post} />}
-        renderEmpty={() => <div>포스트가 없습니다.</div>}
-      />
-    </Stack>
+    <ChakraMotion initial={{ opacity: 0 }} animate={{ opacity: 1 }} exit={{ opacity: 0 }}>
+      <Stack>
+        <List
+          gap="24px"
+          items={allPosts}
+          renderItem={(post) => <PostCard key={post.slug} post={post} />}
+          renderEmpty={() => <div>포스트가 없습니다.</div>}
+        />
+      </Stack>
+    </ChakraMotion>
   )
 }
