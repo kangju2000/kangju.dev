e810491eabe59037883b10f0fa443dc7fb1cfc4f
kang <kangju2000@naver.com>
2023-10-03 00:30:36 +0900

    :lipstick: posts 페이지 UI 변경

--- src/app/posts/page.tsx
+++ src/app/posts/page.tsx
@@ -1,4 +1,4 @@
-import { Stack } from '@chakra-ui/react'
+import { Box, Heading, Spacer, Stack, Text } from '@chakra-ui/react'
 import { allPosts } from 'contentlayer/generated'
 
 import PostCard from './components/PostCard'
@@ -8,7 +8,14 @@ import List from '@/components/List/List'
 export default function PostsPage() {
   return (
     <ChakraMotion initial={{ opacity: 0 }} animate={{ opacity: 1 }} exit={{ opacity: 0 }}>
-      <Stack>
+      <Stack spacing="24px">
+        <Box>
+          <Heading as="h1" fontSize="3xl" mb="8px">
+            포스트
+          </Heading>
+          <Text>개발 관련 포스트를 작성합니다.</Text>
+        </Box>
+        <Spacer />
         <List
           gap="24px"
           items={allPosts}
