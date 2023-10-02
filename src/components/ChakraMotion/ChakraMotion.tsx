78b143276265b1b23ddb22c2a888a15d1d781bce
kang <kangju2000@naver.com>
2023-10-03 00:31:19 +0900

    :bug: transition prop 타입 에러 수정

--- src/components/ChakraMotion/ChakraMotion.tsx
+++ src/components/ChakraMotion/ChakraMotion.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import { chakra, shouldForwardProp } from '@chakra-ui/react'
+import { type ChakraProps, chakra, shouldForwardProp } from '@chakra-ui/react'
 import { isValidMotionProp, motion, type MotionProps } from 'framer-motion'
 
 import type { ComponentPropsWithoutRef } from 'react'
@@ -9,7 +9,7 @@ const ChakraMotion = <C extends React.ElementType>({
   as,
   children,
   ...props
-}: ComponentPropsWithoutRef<C> & { as?: C } & MotionProps) => {
+}: ComponentPropsWithoutRef<C> & { as?: C } & MotionProps & Omit<ChakraProps, 'transition'>) => {
   const Component = as || 'div'
 
   const MotionComponent = chakra(motion(Component), {
