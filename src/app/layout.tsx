211e7bbdb714bded7f8bdb9f09e0a405ac91a5ad
kang <kangju2000@naver.com>
2023-10-03 01:14:36 +0900

    :bug: 빌드 오류 해결

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,11 +1,11 @@
 import { Box, ColorModeScript, type ThemeConfig } from '@chakra-ui/react'
+import { Suspense, type PropsWithChildren } from 'react'
 
 import GoogleAnalytics from './GoogleAnalytics'
 import Providers from './Providers'
 import Header from '@/components/Header/Header'
 
 import type { Metadata } from 'next'
-import type { PropsWithChildren } from 'react'
 
 import '@/styles/globals.css'
 import '@/styles/calendar.css'
@@ -24,23 +24,25 @@ export default function RootLayout({ children }: PropsWithChildren) {
   return (
     <html lang="ko" suppressHydrationWarning>
       <body suppressHydrationWarning>
-        <ColorModeScript initialColorMode={config.initialColorMode} />
-        {process.env.NODE_ENV === 'production' && <GoogleAnalytics />}
-        <Providers>
-          <Header />
-          <Box
-            as="main"
-            maxW="700px"
-            minH="100%"
-            m="0 auto"
-            p={{
-              base: '50px 16px',
-              md: '50px 24px',
-            }}
-          >
-            {children}
-          </Box>
-        </Providers>
+        <Suspense fallback={null}>
+          <ColorModeScript initialColorMode={config.initialColorMode} />
+          {process.env.NODE_ENV === 'production' && <GoogleAnalytics />}
+          <Providers>
+            <Header />
+            <Box
+              as="main"
+              maxW="700px"
+              minH="100%"
+              m="0 auto"
+              p={{
+                base: '50px 16px',
+                md: '50px 24px',
+              }}
+            >
+              {children}
+            </Box>
+          </Providers>
+        </Suspense>
       </body>
     </html>
   )
