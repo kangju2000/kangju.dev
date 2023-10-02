be2e7eeded3da6e4dffa100f5e8bebfd6b5a5a30
kang <kangju2000@naver.com>
2023-10-03 01:13:57 +0900

    :recycle: log 글 페이지를 [slug]의 페이지로 변경

--- src/app/log/[slug]/page.tsx
+++ src/app/log/[slug]/page.tsx
@@ -1,41 +1,17 @@
-import { Box, Stack } from '@chakra-ui/react'
 import { allLogs } from 'contentlayer/generated'
-import { format } from 'date-fns'
-import fs from 'fs'
-import Link from 'next/link'
 
-import Empty from './components/Empty'
-import List from '@/components/List/List'
+import MDXLog from '@/layouts/MDXLog'
 
 export function generateStaticParams() {
-  const monthList = fs.readdirSync('contents/log')
-
-  return monthList.map((month) => ({
-    slug: month,
+  return allLogs.map((log) => ({
+    slug: log.dateFormatted,
   }))
 }
 
-interface MonthPageProps {
-  params: {
-    slug: string
-  }
-}
+export default function LogDetailPage({ params: { slug } }: { params: { slug: string } }) {
+  const log = allLogs.find((log) => log.dateFormatted === slug)
 
-export default function MonthPage({ params: { slug } }: MonthPageProps) {
-  const monthLogs = allLogs.filter((log) => log.yearMonth === slug)
+  if (!log) return <div>not found</div>
 
-  return (
-    <Stack>
-      <List
-        gap="12px"
-        items={monthLogs}
-        renderItem={(log) => (
-          <Box as="li" borderRadius="16px">
-            <Link href={`/${log.slug}`}>{format(new Date(log.date), 'yy.MM.dd')}</Link>
-          </Box>
-        )}
-        renderEmpty={() => <Empty />}
-      />
-    </Stack>
-  )
+  return <MDXLog log={log} />
 }
