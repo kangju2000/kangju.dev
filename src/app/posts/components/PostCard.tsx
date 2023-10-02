c85284598ef1c5c448fa2b4523cf3e24409601d1
kang <kangju2000@naver.com>
2023-10-03 04:23:20 +0900

    :lipstick: post 애니메이션 추가

--- src/app/posts/components/PostCard.tsx
+++ src/app/posts/components/PostCard.tsx
@@ -1,4 +1,4 @@
-import { Box, Flex, Heading, Stack, Text } from '@chakra-ui/react'
+import { Box, Heading, Stack, Text } from '@chakra-ui/react'
 import { format } from 'date-fns'
 import Image from 'next/image'
 import Link from 'next/link'
--- src/app/posts/page.tsx
+++ src/app/posts/page.tsx
@@ -1,4 +1,4 @@
-import { Box, Heading, Spacer, Stack, Text } from '@chakra-ui/react'
+import { Heading, Spacer, Stack, Text } from '@chakra-ui/react'
 import { allPosts } from 'contentlayer/generated'
 
 import PostCard from './components/PostCard'
@@ -7,22 +7,26 @@ import List from '@/components/List/List'
 
 export default function PostsPage() {
   return (
-    <ChakraMotion initial={{ opacity: 0 }} animate={{ opacity: 1 }} exit={{ opacity: 0 }}>
-      <Stack spacing="24px">
-        <Box>
-          <Heading as="h1" fontSize="3xl" mb="8px">
-            포스트
-          </Heading>
-          <Text>개발 관련 포스트를 작성합니다.</Text>
-        </Box>
-        <Spacer />
+    <Stack spacing="24px">
+      <ChakraMotion initial={{ opacity: 0 }} animate={{ opacity: 1 }} exit={{ opacity: 0 }}>
+        <Heading as="h1" fontSize="3xl" mb="8px">
+          포스트
+        </Heading>
+        <Text>개발 관련 포스트를 작성합니다.</Text>
+      </ChakraMotion>
+      <Spacer />
+      <ChakraMotion
+        initial={{ x: -20, opacity: 0 }}
+        animate={{ x: 0, opacity: 1 }}
+        exit={{ opacity: 0 }}
+      >
         <List
           gap="32px"
           items={allPosts}
           renderItem={(post) => <PostCard key={post.slug} post={post} />}
           renderEmpty={() => <div>포스트가 없습니다.</div>}
         />
-      </Stack>
-    </ChakraMotion>
+      </ChakraMotion>
+    </Stack>
   )
 }
