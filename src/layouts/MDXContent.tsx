36c2a1fe7b1c1f197e58c2305e8cba9e5e67189d
kang <kangju2000@naver.com>
2023-10-18 01:43:43 +0900

    :sparkles: mdx 컴포넌트에 next image 및 Admonition 추가

--- src/layouts/MDXContent.tsx
+++ src/layouts/MDXContent.tsx
@@ -2,8 +2,10 @@
 
 import { chakra } from '@chakra-ui/react'
 import { Prose } from '@nikolovlazar/chakra-ui-prose'
+import Image from 'next/image'
 import { useMDXComponent } from 'next-contentlayer/hooks'
 
+import Admonition from '@/components/Admonition/Admonition'
 import Youtube from '@/components/Youtube/Youtube'
 
 import type { MDXComponents } from 'mdx/types'
@@ -12,6 +14,21 @@ const components: MDXComponents = {
   Youtube,
   code: (props) => <chakra.span fontWeight="bold" {...props} />,
   a: (props) => <chakra.a {...props} />,
+  Admonition,
+  Image: (props) => (
+    <Image
+      width={0}
+      height={0}
+      sizes="100vw"
+      style={{
+        width: props.width || '100%',
+        height: props.height || 'auto',
+        borderRadius: '8px',
+        margin: '24px 0',
+      }}
+      {...props}
+    />
+  ),
 }
 
 interface MDXContentProps {
