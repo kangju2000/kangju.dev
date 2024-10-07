ebc27b9400fe5f10c747ac3fdcb8ab51afe47a08
kang <kangju2000@gmail.com>
2024-10-07 22:39:44 +0900

    refactor: giscus 컴포넌트로 교체

--- src/components/common/Giscus/index.tsx
+++ src/components/common/Giscus/index.tsx
@@ -1,43 +1,28 @@
 'use client'
 
 import { useColorMode } from '@chakra-ui/react'
-import { useEffect, useRef } from 'react'
+import GiscusReact from '@giscus/react'
 
-const Giscus = () => {
-  const ref = useRef<HTMLDivElement>(null)
+import type { GiscusProps } from '@giscus/react'
+
+const Giscus: React.FC = () => {
   const { colorMode } = useColorMode()
 
-  useEffect(() => {
-    if (!ref.current) return
+  const giscusConfig: GiscusProps = {
+    repo: 'kangju2000/kangju.dev',
+    repoId: 'R_kgDOKXQbJQ',
+    category: 'General',
+    categoryId: 'DIC_kwDOKXQbJc4CZ4HM',
+    mapping: 'pathname',
+    strict: '0',
+    reactionsEnabled: '1',
+    emitMetadata: '0',
+    inputPosition: 'bottom',
+    lang: 'ko',
+    theme: colorMode,
+  }
 
-    const script = document.createElement('script')
-    script.src = 'https://giscus.app/client.js'
-    script.setAttribute('data-repo', 'kangju2000/kangju.dev')
-    script.setAttribute('data-repo-id', 'R_kgDOKXQbJQ')
-    script.setAttribute('data-category', 'General')
-    script.setAttribute('data-category-id', 'DIC_kwDOKXQbJc4CZ4HM')
-    script.setAttribute('data-mapping', 'pathname')
-    script.setAttribute('data-strict', '0')
-    script.setAttribute('data-reactions-enabled', '1')
-    script.setAttribute('data-emit-metadata', '0')
-    script.setAttribute('data-input-position', 'bottom')
-    script.setAttribute('data-theme', colorMode)
-    script.setAttribute('data-lang', 'ko')
-    script.setAttribute('cross-origin', 'anonymous')
-    script.async = true
-
-    ref.current?.appendChild(script)
-  }, [])
-
-  useEffect(() => {
-    const iframe = document.querySelector<HTMLIFrameElement>('iframe.giscus-frame')
-    iframe?.contentWindow?.postMessage(
-      { giscus: { setConfig: { theme: colorMode } } },
-      'https://giscus.app'
-    )
-  }, [colorMode])
-
-  return <section ref={ref} />
+  return <GiscusReact {...giscusConfig} />
 }
 
 export default Giscus
